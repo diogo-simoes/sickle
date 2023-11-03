6dd4d20a7f0d0805b8a74339e86ceccbcd6babee
Diogo Simoes <diogo.simoes@honeysales>
2023-11-03 23:12:10 +0100

    Sickle UI now provides basic functionality to start and stop an experiment, set the number of comrades participating and list the wallets. Super crude interface, no styling, no ui taken into consideration. Just data wiring.

--- src/client/App.js
+++ src/client/App.js
@@ -1,28 +1,50 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
   const [population, setPopulation] = useState(10);
   const [running, setRunning] = useState(false);
+  const [comrades, setComrades] = useState([]);
 
   const start = () => {
-    setRunning(true);
-    console.log(`Starting new sickle experiment with ${population} comrades`);
+    axios.post('http://localhost:3000' + '/start', {
+      population
+    }).then( (res) => {
+      setRunning(true);
+      console.log(`Starting new sickle experiment with ${population} comrades`);
+    }).catch( (err) => {
+      console.err(err);
+    });
   }
 
   const stop = () => {
-    setRunning(false);
-    console.log('Stopping sickle experiment');
+    axios.post('http://localhost:3000' + '/stop', {
+    }).then( (res) => {
+      setRunning(false);
+      console.log('Stopping sickle experiment');
+    }).catch( (err) => {
+      console.err(err);
+    });
   }
 
   const update = () => {
-    console.log('Updating wallets...');
+    axios.post('http://localhost:3000' + '/update', {
+    }).then( (res) => {
+      setComrades(res.data.comrades);
+    }).catch( (err) => {
+      console.log(err);
+    });
   }
 
   return (
-    <Fragment>
+    <>
       <h1>Sickle UI</h1>
       <div className="setup">
-          <input name="comrades" defaultValue={population} />
+          <input name="comrades" defaultValue={population}
+            onChange={(e) => {
+              setPopulation(e.target.value);
+            }
+          }/>
           <button name="start" type="button"
             disabled={running}
             onClick={start}>
@@ -39,6 +61,11 @@ export default function App() {
               Update
           </button>
       </div>
-    </Fragment>
+      <ul>
+        {comrades.map( (comrade) => 
+          <li key={comrade.id}>Comrade#{comrade.id} ¢{comrade.wallet}</li>
+        )}
+      </ul>
+    </>
   );
 }
